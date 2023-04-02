afc7ecb184b58fd481ae4011ae6111c0be9c7612
M.S.T.O.P <mstop4@gmail.com>
2023-04-02 12:31:16 -0400

    change lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,12 +2,8 @@ module.exports = {
   // Type check TypeScript files
   '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
 
-  // Lint & Prettify TS and JS files
-  '**/*.(ts|tsx|js)': filenames => [
+  '**/*.*': () => [
     `npm run lint`,
-    `npx prettier --write ${filenames.join(' ')}`
+    `npx prettier --write .`
   ],
-
-  // Prettify only Markdown and JSON files
-  '**/*.(md|json)': filenames => `npx prettier --write ${filenames.join(' ')}`
 };
