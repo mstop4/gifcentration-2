53d1a97c7b9fb380f04aa98f59f6ec46da439507
M.S.T.O.P <mstop4@gmail.com>
2023-04-02 16:58:11 -0400

    experiment with CSS

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,10 @@
 import React, { ReactNode } from 'react';
+import styles from '@/styles/Footer.module.css';
 
 type Props = {
   children: ReactNode;
 };
 
-const Footer: React.FC = () => <footer>2023</footer>;
+const Footer: React.FC = () => <footer className={styles.footer}>2023</footer>;
 
 export default Footer;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,12 @@
 import React, { ReactNode } from 'react';
+import styles from '@/styles/Header.module.css';
 
 type Props = {
   children: ReactNode;
 };
 
-const Header: React.FC = () => <header>GIFcentration 2</header>;
+const Header: React.FC = () => (
+  <header className={styles.header}>GIFcentration 2</header>
+);
 
 export default Header;
