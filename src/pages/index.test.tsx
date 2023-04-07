990676f93aa92e27221681040b3ee536a7a56710
M.S.T.O.P <mstop4@gmail.com>
2023-04-07 18:48:51 -0400

    move tests to same folder as components

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import Home from '../src/pages/index';
+import Home from './index';
 import '@testing-library/jest-dom';
 
 describe('Home', () => {
