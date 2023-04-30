f223be2fbf80496c213edae6385a20a00cfa14e5
M.S.T.O.P <mstop4@gmail.com>
2023-04-29 20:01:45 -0400

    prevent redis from reconnect every time app recompiles on dev env

--- lib/redis.ts
+++ lib/redis.ts
@@ -2,33 +2,33 @@
 import { createClient } from 'redis';
 import type { RedisClientType } from 'redis';
 
-let redisClient: RedisClientType;
-let isReady = false;
+const globalForRedis = global as unknown as { redisClient: RedisClientType };
 
 async function getCache(): Promise<RedisClientType> {
-  if (!isReady) {
-    redisClient = createClient({
-      password: process.env.REDIS_PASSWORD,
-      socket: {
-        host: process.env.REDIS_HOST ?? 'locahost',
-        port: parseInt(process.env.REDIS_PORT ?? '6379'),
-      },
-    });
-    redisClient.on('error', err => console.log(`Redis Error: ${err}`));
-    redisClient.on('connect', () => console.log('Redis connected'));
-    redisClient.on('reconnecting', () => console.log('Redis reconnecting'));
-    redisClient.on('ready', () => {
-      isReady = true;
-      console.log('Redis ready!');
-    });
-    await redisClient.connect();
-  }
+  const redisClient: RedisClientType = createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+      host: process.env.REDIS_HOST ?? 'locahost',
+      port: parseInt(process.env.REDIS_PORT ?? '6379'),
+    },
+  });
+  redisClient.on('error', err => console.log(`Redis Error: ${err}`));
+  redisClient.on('connect', () => console.log('Redis connected'));
+  redisClient.on('reconnecting', () => console.log('Redis reconnecting'));
+  redisClient.on('ready', () => {
+    console.log('Redis ready!');
+  });
+  await redisClient.connect();
   return redisClient;
 }
 
 getCache()
   .then(client => {
-    redisClient = client;
+    if (process.env.NODE_ENV !== 'production') {
+      if (!globalForRedis.redisClient) {
+        globalForRedis.redisClient = client as RedisClientType;
+      }
+    }
   })
   .catch(err => {
     console.log({ err }, 'Failed to connect to Redis');
