d5ed81f4d2398e51a34c9ca9704fc88d6a815c6d
M.S.T.O.P <mstop4@gmail.com>
2023-05-12 19:18:25 -0400

    update lintstaged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   // Type check TypeScript files
   '**/*.(ts|tsx)': () => 'npm run check-ts',
 
-  '**/*.*': () => [
+  '**/*.(ts|tsx|scss)': () => [
     `npm run lint`,
     `npm run prettier`,
     `npm run test`
