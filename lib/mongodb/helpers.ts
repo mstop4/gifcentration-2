d9603a52fe9b25a43151418aaf944c304e5f26c3
M.S.T.O.P <mstop4@gmail.com>
2023-12-04 19:34:36 -0500

    make getTopSearches accept searches in old schema

--- lib/mongodb/helpers.ts
+++ lib/mongodb/helpers.ts
@@ -9,7 +9,9 @@ export type TopSearchResult = {
 
 export async function getTopSearches(): Promise<TopSearchResult[]> {
   await dbConnect();
-  const topSearches = await Search.aggregate([{ $match: { isObscene: false } }])
+  const topSearches = await Search.aggregate([
+    { $match: { isObscene: { $ne: true } } },
+  ])
     .group({ _id: '$query', count: { $sum: 1 } })
     .sort({ count: -1 })
     .limit(serverConfig.home.topSearchesLimit);
