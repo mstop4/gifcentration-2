2e8b7d47337c4624a7a631d5921eaf13a6a4adf6
M.S.T.O.P. <mstop4@gmail.com>
2026-03-10 12:43:13 -0400

    fixed warnings caused by migration from Next 14 to 16

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverComponentsExternalPackages: ['mongoose'], // https://stackoverflow.com/questions/75697312/import-mongoose-lib-in-api-directory-in-next-js-13-2-app-directory-gives-error
   reactStrictMode: true,
   sassOptions: {
     includePaths: [join(__dirname, 'styles')],
